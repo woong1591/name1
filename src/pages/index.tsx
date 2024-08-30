ae8ec37ddf8d0340afb72669cbeb50710733a3ad
woong1591 <kwh9388@gmail.com>
2024-08-31 01:16:14 +0900

    test: commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import HomePage from "@/components/home/HomePage";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
+      <h1 className="text-3xl font-bold">
         Hello world!
       </h1>
       <HomePage />
